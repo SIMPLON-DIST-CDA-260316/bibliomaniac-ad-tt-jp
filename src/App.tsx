2fa00c4a674fb9ee65044eb3aba5bae6af1553d2
Jordan <jordan.p62@outlook.fr>
2026-04-16 11:55:40 +0200

    project setup : added tailwind variables and a default homepage to make the router work

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router";
+import Homepage from "./pages/Homepage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={""} />
+      <Route path="/" element={<Homepage />} />
     </Routes>
   );
 }
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,7 @@
+export default function Homepage() {
+  return (
+    <>
+      <h1 className="font-title text-accent">Home</h1>
+    </>
+  );
+}
