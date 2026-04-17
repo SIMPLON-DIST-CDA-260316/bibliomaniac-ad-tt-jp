21556ff434211af70f05d12321e2e7a4314cfb3f
Your Name <you@example.com>
2026-04-17 10:23:28 +0200

    component: header - taille modifiee

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,6 @@
-import Header from "./components/Header";
-
 export default function Homepage() {
   return (
     <>
-      <Header />
       <h1 className="font-title text-accent">Home</h1>
     </>
   );
